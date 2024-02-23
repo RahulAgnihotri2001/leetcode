6f0a6a68b2d4b5a5c93f06a60a979e46bfd3c44a
RahulAgnihotri2001 <100308406+RahulAgnihotri2001@users.noreply.github.com>
2024-02-23 09:41:46 +0530

    Time: 21 ms (37.81%) | Memory: 16.6 MB (51.70%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // make map directed
-        map<int,vector<pair<int,int>>> mp;  // src,destination , dis
-        for(int i=0;i<flights.size();i++){
-            mp[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flight, int src, int dst, int k) {
+        // make priority queue with number of stops as deciding factor
+        // will follow dijkestra
+        // {stops{destination,price}};
+        // converting v<v> it to graph
+
+
+        map<int,vector<pair<int,int>>> mp;
+        for(int i=0;i<flight.size();i++){
+            mp[flight[i][0]].push_back({flight[i][1],flight[i][2]});
         }
         queue<pair<int,pair<int,int>>> pq;
-        pq.push({0,{src,0}});   // stops , src ,distance;
+        pq.push({0,{src,0}});
         vector<int> dist(n,1e9);
         dist[src]=0;
         while(!pq.empty()){
-            pair<int,pair<int,int>> temp=pq.front();
+            int stops=pq.front().first;
+            int source=pq.front().second.first;
+            int fair=pq.front().second.second;
             pq.pop();
-            int node = temp.second.first;
-            int distance=temp.second.second;
-            int stop=temp.first;
-            for(auto it : mp[node]){
-                int des= it.first;
-                int dis=it.second;
-                if(dis+distance<dist[des]){
-                    dist[des]=dis+distance;
-                    if(stop+1<=k){
-                    pq.push({stop+1,{des,dis+distance}});
+            for(auto it: mp[source]){
+                int d=fair+it.second;
+                if(d<dist[it.first]){
+                    dist[it.first]=d;
+                    if(stops+1<=k){
+                        pq.push({stops+1,{it.first,d}});
+                    }
                 }
             }
-                
-            }
         }
-        if(dist[dst]==1e9){
-            return -1;
+        if(dist[dst]!=1e9){
+            return dist[dst];
         }
-        return dist[dst];
+        return -1;
     }
 };
