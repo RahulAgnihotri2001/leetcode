206d1f2f5e3c5e190ee65885e306ba6528e12224
RahulAgnihotri2001 <100308406+RahulAgnihotri2001@users.noreply.github.com>
2024-07-04 00:43:18 +0530

    Time: 73 ms (34.80%) | Memory: 38.7 MB (50.84%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -9,5 +9,8 @@ public:
         int b= min((nums[nums.size()-3]-nums[1]),(nums[nums.size()-2]-nums[2]));
         int c=min((nums[nums.size()-2]-nums[2]),(nums[nums.size()-3]-nums[1]));
         return min(c,min(a,b));
+        // This solution have a TC O(nlogn)
+        // We only need 4 smallest and 4 largest element hence no need to sort entire array
+        // Can find them in O(n) using heap of size 4, hence we most optimal solution TC O(n)
     }
 };
