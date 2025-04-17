8ceba8cd8e014219a130e0ac9c892dc6e748e5e9
RahulAgnihotri2001 <100308406+RahulAgnihotri2001@users.noreply.github.com>
2025-04-17 14:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (39.11%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index=m+n-1;
-        int i=m-1;
-        int j=n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]>=nums2[j]){
-                nums1[index]=nums1[i];
-                i--;
-                index--;
+        // nums1 have a length m+n;
+        // Shift nums1 to extrime right
+        for(int i=m-1;i>=0;i--){
+            nums1[i+n]=nums1[i];
+        }
+        int start=0;
+        int i=n;
+        int j=0;
+        while(i<m+n && j<n){
+            if(nums1[i]<=nums2[j]){
+                nums1[start]=nums1[i];
+                i++;
             }
             else{
-                nums1[index]=nums2[j];
-                j--;
-                index--;
+                nums1[start]=nums2[j];
+                j++;
+            }
+            start++;
+        }
+        if(j<n){
+            while(j<n){
+                nums1[start]=nums2[j];
+                j++;
+                start++;
             }
         }
-        if(j>=0){
-            while(j>=0){
-                nums1[index]=nums2[j];
-                j--;
-                index--;
-            }
-        }
-        return;
     }
 };
