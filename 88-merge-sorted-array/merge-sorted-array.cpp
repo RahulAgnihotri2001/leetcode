f76ba3053ad47cd90f51a178b509c7c80b52291a
RahulAgnihotri2001 <100308406+RahulAgnihotri2001@users.noreply.github.com>
2026-07-12 01:43:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (42.40%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // nums1 have a length m+n;
-        // Shift nums1 to extrime right
-        for(int i=m-1;i>=0;i--){
-            nums1[i+n]=nums1[i];
+        if(n==0){
+            return;
         }
-        int start=0;
-        int i=n;
-        int j=0;
-        while(i<m+n && j<n){
-            if(nums1[i]<=nums2[j]){
-                nums1[start]=nums1[i];
-                i++;
+        if(m==0){
+            nums1 = nums2;
+        }
+        for(int i=m-1;i>=0;i--){
+            nums1[n+i]=nums1[i];
+        }
+        int start2=0;
+        int start1=n;
+        for(int i=0;i<(n+m);i++){
+            if(start1>=(m+n) || (start2<n && nums2[start2]<nums1[start1])){
+                nums1[i]=nums2[start2];
+                start2++;
             }
             else{
-                nums1[start]=nums2[j];
-                j++;
-            }
-            start++;
-        }
-        if(j<n){
-            while(j<n){
-                nums1[start]=nums2[j];
-                j++;
-                start++;
+                nums1[i]=nums1[start1];
+                start1++;
             }
         }
     }
