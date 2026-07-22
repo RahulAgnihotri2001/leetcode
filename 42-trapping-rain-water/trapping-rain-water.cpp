52831a386b32cfdd71e4de0ce4afd948d8d5ecc7
RahulAgnihotri2001 <100308406+RahulAgnihotri2001@users.noreply.github.com>
2026-07-23 01:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (6.76%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int arrleft[height.size()];
-        int arrright[height.size()];
-        int mal=0;
+        vector<int> bl;
+        vector<int> br;
+        int a=0;
         for(int i=0;i<height.size();i++){
-            arrleft[i]=mal;
-            mal=max(height[i],mal);
+            bl.push_back(a);
+            a=max(a,height[i]);
         }
-        int mar=0;
+        a=0;
         for(int i=height.size()-1;i>=0;i--){
-            arrright[i]=mar;
-            mar=max(height[i],mar);
+            br.push_back(a);
+            a=max(a,height[i]);
         }
-        /*
+        reverse(br.begin(),br.end());
+        int answer=0;
         for(int i=0;i<height.size();i++){
-            cout<<arrleft[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<height.size();i++){
-            cout<<arrright[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<height.size();i++){
-            cout<<height[i]<<" ";
-        }
-        */
-        int sum=0;
-        for(int i=0;i<height.size();i++){
-            int left=arrleft[i];
-            int right=arrright[i];
-            if(left>height[i] && right>height[i]){
-                sum=sum+min(left,right)-height[i];
+            if(height[i]<min(br[i],bl[i])){
+                answer+= min(br[i],bl[i])-height[i];
             }
         }
-        return sum;
+        return answer;
     }
 };
